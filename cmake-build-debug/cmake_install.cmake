25692ee77bd0b9fc71477004d535a1c6ff708378
pensa1919 <thomas.pensa@gmai.com>
2025-02-16 13:28:33 +0100

    hi

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Public/hu6
+# Install script for directory: /home/thomas/Desktop/FH/SWO/dockerhome/hu62
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/hu6")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -34,7 +39,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files (x86)/JetBrains/CLion 2024.3.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -52,6 +57,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Public/hu6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/thomas/Desktop/FH/SWO/dockerhome/hu62/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.cpp
+++ main.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cassert>
+#include <algorithm>
 
-class input_entry {
+struct input_entry {
     public:
     input_entry(char c): c(c), count(1), p(0) {}
     char c;
     int count;
-    float p;
+    float p = 0;
+    input_entry * left {nullptr};
+    input_entry * right {nullptr};
+
+    bool operator < (input_entry r) {
+        return p > r.p;
+    }
 };
 
 class huffman_list {
     std::vector<input_entry>  v;
-    int sign_count;
     void calculate_sign_count() {
         sign_count = 0;
         for (int i = 0; i < v.size(); i++, sign_count += v[i].count);
     }
-    int calculate_p () {
-        for (int i = 0; i < v.size(); i++)
-            v[i].p = v[i].count / sign_count;
+    void calculate_p () {
+        assert (sign_count != 0);
+        for (int i = 0; i < v.size(); i++) {
+            v[i].p = (float)v[i].count / sign_count;
+        }
     }
     public:
-    huffman_list(std::string filename) {
+    int sign_count;
+
+    huffman_list(std::string filename): sign_count{0} {
         char c;
         int found_pos;
 
@@ -50,22 +61,94 @@ class huffman_list {
         }
         calculate_sign_count();
         calculate_p();
+        std::sort (v.begin(), v.end());
     }
 
     void print_list () {
-        std::cout << "ich wurde ausgeührt" << v.size() << std::endl;
         for (int i = 0; i < v.size(); i++) {
-            std::cout << v[i].c << "  ::  " << v[i].count <<  v[i].p << std::endl;
+            std::cout << v[i].c << "  ::  " << v[i].count << "von "<<  v[i].p << std::endl;
         }
     }
+    input_entry * operator [] (int i){
+        return &v[i];
+    }
+
+
 };
 
+class huffman_tree {
+    struct node {
+        input_entry * data;
+        node * next {nullptr};
+    };
+    node * head {nullptr};
 
+public:
+    huffman_tree (huffman_list l) {
+        for (int i = 0; i < l.sign_count; i++) {
+            node * new_node = new node();
+            new_node->data = l[i];
+            if (!head)
+                head = new_node;
+            else {
+                new_node->next = head;
+                head = new_node;
+            }
+        }
+
+
+        while (head->next && head->next->next) {
+            input_entry * d1 = head->data;
+            input_entry * d2 = head->next->data;
+            node *new_node = new node();
+            new_node->data = new input_entry('-');
+            new_node->data->count = d1->count + d2->count;
+            if (d1->count < d2->count) {
+                new_node->data->left = d1;
+                new_node->data->right = d2;
+            }
+            else {
+                new_node->data->left = d2;
+                new_node->data->right = d1;
+            }
+
+            node * rem1 {head}; node * rem2 {head->next};
+            delete rem1; delete rem2;
+            head = head -> next -> next;
+
+            node * n = head;
+            while (n->next && n->next->data->count < new_node->data->count)
+                n = n->next;
+
+            new_node->next = n->next;
+            n->next = new_node;
+
+
+        }
+
+            if (head->next) {
+                input_entry * d1 = head->data;
+                node * tmp {head};
+                head = head ->next;
+                delete tmp;
+
+                input_entry * new_entry;
+                new_entry->c = '-';
+                new_entry->count = d1->count + head->data->count;
+            }
+
+
+
+
+    }
+
+};
 
 
 
 int main() {
     huffman_list h("input.txt");
     h.print_list();
+    std::cout << h.get_entry().c << std::endl;
     return 0;
 }

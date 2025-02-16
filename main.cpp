62d5e0086b7c62a1434ccc3089ac77d03114fa55
pensa1919 <thomas.pensa@gmail.com>
2025-02-16 09:14:07 +0100

    new

--- main.cpp
+++ main.cpp
@@ -2,56 +2,70 @@
 #include <vector>
 #include <fstream>
 
-struct input_entry {
+class input_entry {
+    public:
+    input_entry(char c): c(c), count(1), p(0) {}
     char c;
     int count;
+    float p;
 };
 
-bool get_char(std::string filename, char & c) {
-    std::ifstream file(filename);
-    if (!file)
-        return false;
-    file.get(c);
-    return true;
-}
+class huffman_list {
+    std::vector<input_entry>  v;
+    int sign_count;
+    void calculate_sign_count() {
+        sign_count = 0;
+        for (int i = 0; i < v.size(); i++, sign_count += v[i].count);
+    }
+    int calculate_p () {
+        for (int i = 0; i < v.size(); i++)
+            v[i].p = v[i].count / sign_count;
+    }
+    public:
+    huffman_list(std::string filename) {
+        char c;
+        int found_pos;
 
-void create_list (std::string filename, std::vector<input_entry> & v) {
-    char c;
-    int found_pos;
-    while (get_char(filename, c)){
-        found_pos = -1;
-        for (int i = 0; i < v.size(); i++) {
-            if (v[i].c == c) {
-                found_pos = i;
-                break;
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Fehler: Datei konnte nicht geöffnet werden." << std::endl;
+            return;
+        }
+        while (file.get(c)){
+            found_pos = -1;
+            for (int i = 0; i < v.size(); i++) {
+                if (v[i].c == c) {
+                    found_pos = i;
+                    break;
+                }
+            }
+            if (found_pos != -1) {
+                v[found_pos].count++;
+                found_pos = -1;
+            }
+            else {
+                input_entry entry(c);
+                v.push_back(entry);
             }
         }
-        if (found_pos != -1) {
-            v[found_pos].count++;
-            found_pos = -1;
-        }
-        else {
-            input_entry entry;
-            entry.c = c;
-            entry.count = 1;
-            v.push_back(entry);
-        }
-
+        calculate_sign_count();
+        calculate_p();
     }
-}
 
-void print_list (std::vector<input_entry> & v) {
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i].c << "  ::  " << v[i].count << std::endl;
+    void print_list () {
+        std::cout << "ich wurde ausgeührt" << v.size() << std::endl;
+        for (int i = 0; i < v.size(); i++) {
+            std::cout << v[i].c << "  ::  " << v[i].count <<  v[i].p << std::endl;
+        }
     }
-}
+};
+
+
+
 
 
 int main() {
-    std::vector<input_entry> v;
-    create_list ("input.txt", v);
-    print_list(v);
-
-
+    huffman_list h("input.txt");
+    h.print_list();
     return 0;
 }
